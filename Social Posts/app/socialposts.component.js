3f42d1243c8bd6468d9a1dc708736454a7a82fba
rmcleod2378 <44953292+rmcleod2378@users.noreply.github.com>
2019-02-13 21:39:03 -0500

    Some style changes

--- Social Posts/app/socialposts.component.js
+++ Social Posts/app/socialposts.component.js
@@ -6,9 +6,9 @@ const socialposts = {
         const vm = this;
         vm.formToggle = false;
         vm.postLibrary = [
-            {title: "Today", thought: "Awesome"},
-            {title: "Tomorrow", thought: "Moving On"},
-            {title: "Yesterday", thought: "Forget It"}
+            {title: "Today", thought: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptate debitis, reprehenderit labore sed dolores tempore cumque commodi vel nesciunt, illo ex magni beatae culpa unde esse temporibus corporis! Vero."},
+            {title: "Tomorrow", thought: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptate debitis, reprehenderit labore sed dolores tempore cumque commodi vel nesciunt, illo ex magni beatae culpa unde esse temporibus corporis! Vero."},
+            {title: "Yesterday", thought: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laboriosam nostrum sint suscipit consectetur consequatur repellendus ullam nemo ipsam voluptates eveniet, quae tenetur ipsum. Ullam quisquam cum possimus minus laudantium."}
             
         ];
         vm.addPost = function(newPost){
